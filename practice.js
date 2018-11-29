678821055771c56de278a8a14a8fffd19999989a
Allen Carlson <allen.carlson123@gmail.com>
2018-11-28 19:24:20 -0700

    completed

--- practice.js
+++ practice.js
@@ -178,50 +178,50 @@
 
 // ////////// PROBLEM 8 //////////
 
-// // Do not edit the code below.
-// var myGroceryList = ['chips', 'pizza', 'hotpockets', 'MtnDew', 'corndogs'];
-// // Do not edit the code above.
+// Do not edit the code below.
+var myGroceryList = ['chips', 'pizza', 'hotpockets', 'MtnDew', 'corndogs'];
+// Do not edit the code above.
 
-// /*
-//   Here we're going to write a function that mimics going shopping and checking things off of our grocery list and adding new items to our list.
+/*
+  Here we're going to write a function that mimics going shopping and checking things off of our grocery list and adding new items to our list.
 
-//   Write a function called removeItem that is given two arguments, the first is myGroceryList, and the second is an item to remove from myGroceryList. 
-//   If the second argument (or the item to add or remove) matches an item in myGroceryList, remove that item from the your grocery list and return the new, updated grocery list.
+  Write a function called removeItem that is given two arguments, the first is myGroceryList, and the second is an item to remove from myGroceryList. 
+  If the second argument (or the item to add or remove) matches an item in myGroceryList, remove that item from the your grocery list and return the new, updated grocery list.
 
-//   Once you do that, write another function called addItem that is given two arguments, the first is myGroceryList and the second is an item to add to your grocery list. 
-//   In addItem add the item you passed in to myGroceryList then return the new, updated grocery list.
+  Once you do that, write another function called addItem that is given two arguments, the first is myGroceryList and the second is an item to add to your grocery list. 
+  In addItem add the item you passed in to myGroceryList then return the new, updated grocery list.
 
-//   In both removeItem and addItem check to see if the 'myGroceryList' and 'item' arguments are truthy.
-//   If they are not, return an empty array.
+  In both removeItem and addItem check to see if the 'myGroceryList' and 'item' arguments are truthy.
+  If they are not, return an empty array.
 
-//   Here are some examples of calling your functions and what should be returned:
-//   removeItem(myGroceryList, 'chips') --> ['pizza', 'hotpockets', 'MtnDew', 'corndogs'];
-//   addItem(myGroceryList, 'Jerky') --> ['pizza', 'hotpockets', 'MtnDew', 'corndogs', 'Jerky'];
-//   removeItem(myGroceryList) --> [];
-//   addItem() --> [];
-// */
+  Here are some examples of calling your functions and what should be returned:
+  removeItem(myGroceryList, 'chips') --> ['pizza', 'hotpockets', 'MtnDew', 'corndogs'];
+  addItem(myGroceryList, 'Jerky') --> ['pizza', 'hotpockets', 'MtnDew', 'corndogs', 'Jerky'];
+  removeItem(myGroceryList) --> [];
+  addItem() --> [];
+*/
 
-// //Code Here
-// itemRemoved = [];
-// function removeItem(myGroceryList, item) {
-//   if(!myGroceryList  || !item) {
-//     return []
-//   }
-//   for (let i = 0; i < myGroceryList.length; i++) {
-//     if (myGroceryList[i] === item) {
-//       myGroceryList.splice(i, 1)
-//     }
-//   }
-//   return myGroceryList
-// }
+//Code Here
+itemRemoved = [];
+function removeItem(myGroceryList, item) {
+  if(!myGroceryList  || !item) {
+    return []
+  }
+  for (let i = 0; i < myGroceryList.length; i++) {
+    if (myGroceryList[i] === item) {
+      myGroceryList.splice(i, 1)
+    }
+  }
+  return myGroceryList
+}
 
-// function addItem(myGroceryList, item) {
-//   if(!myGroceryList || !item) {
-//     return []
-//   }
-//     myGroceryList.push(item)
-//     return myGroceryList
-// }
+function addItem(myGroceryList, item) {
+  if(!myGroceryList || !item) {
+    return []
+  }
+    myGroceryList.push(item)
+    return myGroceryList
+}
 
 
 
